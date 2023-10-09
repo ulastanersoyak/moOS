cce4718caf8f63bd19e85e639bd7c711c3ccc5db
ulastanersoyak <ulastanersoyak@hotmail.com>
2023-10-09 11:06:14 +0300

    gdt

--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -1,28 +1,16 @@
 [org 0x7c00]
 
-mov bp, 0x8000 ;set stack away from bootloader
-mov sp, bp
+cli ;clear interrupt. cpu will ignore any future interrupts that may interrupt mode switching.interrupt handling is different from real mode on protected     ;mode. current interrupt vector table set by bios which will run 16 bit code, which cant handle 32 bit segments defined in gdt
+lgdt [gdt_descriptor] ;load gdt instruction. which will take address of descriptor
+;making actual switch by setting the first bit of a special cpu control register called cr0.
 
-mov bx, 0x9000 ;es:bx 0x0000:0x9000
-mov dh, 2 ;read 2 sectors 
-
-call disk_load_bios
-
-mov dx, [0x9000]
-call print_hex_bios
-
-mov dx, [0x9000 + 512]
-call print_hex_bios
 
 jmp $
 
-%include "./src/bootloader/print_str_bios.asm" ;includes print_str subroutin
-%include "./src/bootloader/print_hex_bios.asm" ;includes print_hex subroutine
-%include "./src/bootloader/disk_load_bios.asm" ;includes disk_load subroutine 
-
+%include "./src/bootloader/bios_subroutines/print_str_bios.asm" ;includes print_str subroutin
+%include "./src/bootloader/bios_subroutines/print_hex_bios.asm" ;includes print_hex subroutine
+%include "./src/bootloader/bios_subroutines/disk_load_bios.asm" ;includes disk_load subroutine 
+%include "./src/bootloader/global_descriptor_table.asm" ;includes gdt and gdt descriptor for switching to 32 bit protected mode from 16 bit real mode
 times 510 - ($-$$) db 0
 
 dw 0XAA55
-
-times 256 dw 0xdada
-times 256 dw 0xface
---
+++ src/bootloader/pm_subroutines/print_str_pm.asm
@@ -0,0 +1,24 @@
+[bits 32]
+
+video_memory equ 0xb800
+monochrome equ 0x0f
+
+print_str_pm:
+  ;print a string in protected mode. uses edx as parameter
+  pusha
+  mov edx, video_memory
+_str_pm_loop:
+  mov al, [ebx] ;video graphic array uses a registers low bits as char
+  mov ah, monochrome ;and high bits as metadata about char like background and foreground colours of char
+
+  cmp al, 0 ;check if string is ended
+  je _str_pm_return
+  
+  mov [edx], ax ;store char and attributes at current char cell
+  add ebx, 1 ;increment ebx to next char on string
+  add edx, 2 ;move to next char in video memory
+  jmp _str_pm_loop
+
+_str_pm_return:
+  popa
+  ret
